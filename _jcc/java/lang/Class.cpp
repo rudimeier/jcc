3a6b4b070f47b721c0f1069a15e43e9251f22a61
Ruediger Meier <ruediger.meier@ga-group.nl>
2017-03-09 02:55:45 +0100

    fix function t_Class_forName()
    Whatever this is good for ... just imported it from
    http://svn.apache.org/repos/asf/lucene/pylucene/branches/python_3/jcc@963444
    
    Signed-off-by: Ruediger Meier <ruediger.meier@ga-group.nl>

--- _jcc/java/lang/Class.cpp
+++ _jcc/java/lang/Class.cpp
@@ -397,17 +397,14 @@ namespace java {
 
         static PyObject *t_Class_forName(PyTypeObject *type, PyObject *arg)
         {
-            if (!PyString_Check(arg))
+            if (arg == Py_None)
             {
-                PyErr_SetObject(PyExc_TypeError, arg);
+                PyErr_SetObject(PyExc_ValueError, arg);
                 return NULL;
             }
 
             try {
-                char *className = PyString_AsString(arg);
-                String name = String(env->fromUTF(className));
-
-                return t_Class::wrap_Object(Class::forName(name));
+                return t_Class::wrap_Object(Class::forName(p2j(arg)));
             } catch (int e) {
                 switch (e) {
                   case _EXC_JAVA:
