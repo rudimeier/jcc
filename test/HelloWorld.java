871b85a13872e46767fb075dc8a7329929e953bd
Ruediger Meier <ruediger.meier@ga-group.nl>
2017-03-18 16:34:26 +0100

    travis: initial setup

---
+++ test/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, Wörld");
+        System.out.println(args[0]);
+    }
+    public static String get() {
+        return "Hello, Wörld";
+    }
+}
+
